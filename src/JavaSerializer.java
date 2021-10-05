96bfa7d8a9e7e6a56c976a6a63eb716598b63fcb
chbu <chbu@dtu.dk>
2021-10-05 16:50:52 +0200

    Init commit

---
+++ src/JavaSerializer.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class JavaSerializer {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setUserName("testUser");
+        user.setPassWord("testPass");
+        writeToFile(user);
+        System.out.println("Wrote file");
+        System.out.println("Reading file");
+        User loadedUser = readFromFile();
+        System.out.println(loadedUser);
+    }
+
+    private static User readFromFile() {
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("User.obj"));
+            Object o = objectInputStream.readObject();
+            return (User) o;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void writeToFile(Serializable s){
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("User.obj"));
+            objectOutputStream.writeObject(s);
+            objectOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/User.java
@@ -0,0 +1,31 @@
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String userName;
+    private String passWord;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", passWord='" + passWord + '\'' +
+                '}';
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+}
